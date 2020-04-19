2f441eedaa21402c29e86bfbc8e1a59019f245a8
megamp15 <47288803+megamp15@users.noreply.github.com>
2020-04-19 14:13:47 -0500

    Created Files

---
+++ project.py
@@ -0,0 +1,15 @@
+# File: project.py
+# Author(s): Mahir Pirmohammed, Ashok Meyyappan
+# Date: 04/19/2020
+# Section: 510
+# E-mail: megamp15@tamu.edu
+# Description:
+# e.g. The content of this file implements ...
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
