d3dd5021a9131797e4c1d16199a845f54b2473c1
megamp15 <47288803+megamp15@users.noreply.github.com>
2020-04-29 21:07:30 -0500

    Added comments to cachesimulator.py file

--- cachesimulator.py
+++ cachesimulator.py
@@ -1,16 +1,21 @@
 # File: cachesimulator.py
 # Author(s): Mahir Pirmohammed, Ashok Meyyappan
-# Date: 04/19/2020
+# Date: 04/30/2020
 # Section: 510
 # E-mail: megamp15@tamu.edu
 # Description:
-# e.g. The content of this file implements...
+# e.g. The content of this file implements the main functions of the cachesimulator:
+# Initializing the Ram in the init_ram function using the input file from command line argument.
+# Configuring the Cache in the config_cache function by asking input from command line.
+# Simulating the cache in the simulate_cache() function which holds the menu and calls functions in the menu.py.
 
+# Imports
 import argparse
 import math
-import menu
+import menu  # import of menu.py file
 
-RAM = {}
+# Global variables
+RAM = {}  # Dictionary structure of RAM
 m = 8
 count = 0
 
@@ -21,17 +26,18 @@ def init_ram(input):
     print("init-ram 0x00 0xFF")
     i = open(input, "r")
     for l in i.read().splitlines():
+        # Count is converted to hex which is the key in the RAM dictionary with value being the current line l from input.
         RAM["0x"+str(hex(count))[2:].zfill(2)] = l
         count += 1
-    # for r in RAM:
-    #     print(r)
     i.close()
     print("ram successfully initialized!")
 
 
 def config_cache():
+    # Global variables that are used in menu.py functions
     global C, B, E, replace, write_hit, write_miss, S, s, b, t
     print("configure the cache:")
+    # Inputs
     C = int(input("cache size: "))
     B = int(input("data block size: "))
     E = int(input("associativity: "))
@@ -39,6 +45,7 @@ def config_cache():
     write_hit = int(input("write hit policy: "))
     write_miss = int(input("write miss policy: "))
     print("cache successfully configured!")
+    # Computations for menu.py functions
     S = int(C/(B*E))
     s = math.log2(S)
     b = math.log2(B)
@@ -48,7 +55,8 @@ def config_cache():
 
 def simulate_cache():
     command = ""
-    while(command != "quit"):
+    while(command != "quit"):  # Until the command quit is received.
+        # MENU
         print("*** Cache simulator menu ***")
         print("type one command: ")
         print("1. cache-read")
@@ -60,7 +68,9 @@ def simulate_cache():
         print("7. memory-dump")
         print("8. quit")
         print("****************************")
+        # command input
         command = input("")
+        # Calls to menu.py functions and passing command arguments and global variables
         if "cache-read" in command:
             menu.cache_read(command[command.find(
                 " ")+1:].strip(), s, t, b, S, E, B, replace, RAM)
@@ -79,11 +89,15 @@ def simulate_cache():
         elif "memory-dump" in command:
             menu.memory_dump(RAM)
         else:
-            if command != "quit":
+            if command != "quit":  # Incase a incorrect menu command is received.
                 print("Not a command. Please type one command from the menu.")
 
 
 def main():
+    # Main function that is called when running program.
+    # Takes the input file as a command line argument that is passed to init_ram.
+    # Config_cache is then called to trigger input from command line.
+    # Simulate_cache is called to start the menu and do commands until quit command is entered.
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "input",
